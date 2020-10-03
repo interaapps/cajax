75c5abe30752576f01aeeeec45a794494b59a578
Julian Gojani <julian@gojani.xyz>
2020-10-03 17:40:24 +0200

    Removed testing files

--- example_server.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-echo $_SERVER["REQUEST_METHOD"]
-."\n".(($_POST) ? "POST: ".json_encode($_POST):"")
-."\n".(($_GET) ? "GET: ".json_encode($_GET) :"")
-."\n"."HEADERS: ".json_encode(getallheaders());
